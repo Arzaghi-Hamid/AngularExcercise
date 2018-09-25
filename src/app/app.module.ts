239397d2172124282c741ee14fe20803daa65260
Hamid <hamidreza_hobous@yahoo.com>
2018-09-25 14:33:53 -0400

    HackernewsApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsListComponent } from './news-feed/news-list/news-list.component';
+import { NewsDetailComponent } from './news-feed/news-detail/news-detail.component';
+import { NewsCommentComponent } from './news-feed/news-comment/news-comment.component';
+import { NewsFeedService } from './shared/news-feed-service';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    NewsDetailComponent,
+    NewsCommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    NewsFeedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
